5b2500366411f62ed3118a5f76b824de6a3cb6e5
Alyssa Moody and Olena Kuchko <alyssa+olena@>
2017-05-22 10:54:19 -0700

    practice with forms and Nancy

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,28 @@
+using Nancy;
+using MadLibs.Objects;
+
+namespace MadLibs
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/form"] = _ => {
+        return View["form.cshtml"];
+      };
+
+      Get["/story"] = _ => {
+        StoryVariables myStoryVariables = new StoryVariables();
+        myStoryVariables.SetPerson1(Request.Query["person1"]);
+        myStoryVariables.SetPerson2(Request.Query["person2"]);
+        myStoryVariables.SetAnimal(Request.Query["animal"]);
+        myStoryVariables.SetExclamation(Request.Query["exclamation"]);
+        myStoryVariables.SetVerb(Request.Query["verb"]);
+        myStoryVariables.SetNoun(Request.Query["noun"]);
+        myStoryVariables.SetColor(Request.Query["color"]);
+        myStoryVariables.SetAnimal2(Request.Query["animal2"]);
+        return View["story.cshtml", myStoryVariables];
+      };
+    }
+  }
+}
